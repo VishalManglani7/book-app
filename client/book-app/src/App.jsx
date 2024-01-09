9a7300f6336ab0b6ed140bcf0773f12cb91e206e
Vishal <vishal.manglani7@gmail.com>
2024-01-08 19:58:44 -0500

    added react, and built out structure/commented on next steps

---
+++ client/book-app/src/App.jsx
@@ -0,0 +1,20 @@
+// import './App.css'
+import Header from './components/Header'
+// import UserAuth from './components/UserAuth'
+// import MyList from './components/MyList'
+// import UpdateList from './components/UpdateList'
+// import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      {/* <UserAuth />
+      <MyList />
+      <UpdateList/>
+      <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/book-app/src/components/MyList.jsx
@@ -0,0 +1,27 @@
+import Book from "./Book";
+
+function Library() {
+  // fill in the required data, image file should be in the assets/projects folder and name should match the "default as " statment in index.js
+  const books = [
+    
+    {
+    },
+
+  ];
+
+  // for each project, use the Project component to build a project
+  return (
+    <div>
+      <div className="d-flex">
+        {/* if projects is empty -> "Add to your library"
+        else : function that maps out the books added */}
+
+        {books.map((Book) => (
+          <Book book={book} key={"book-" + book.name} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Library;
