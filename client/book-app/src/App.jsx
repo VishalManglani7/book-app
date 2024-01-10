c2a49c55883a5bd12f9609d783a583d8079d81b6
malloryklar0516 <malloryklar@Mallorys-MacBook-Air.local>
2024-01-09 20:42:03 -0500

    added login page and other changes

--- client/book-app/src/App.jsx
+++ client/book-app/src/App.jsx
@@ -1,20 +1,12 @@
 // import './App.css'
-import Header from './components/Header'
-// import UserAuth from './components/UserAuth'
-// import MyList from './components/MyList'
-// import UpdateList from './components/UpdateList'
-// import Footer from './components/Footer'
-
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="main">
       <Header />
-      {/* <UserAuth />
-      <MyList />
-      <UpdateList/>
-      <Footer/> */}
+      {<Outlet />}
     </div>
   );
 }
-
 export default App;
